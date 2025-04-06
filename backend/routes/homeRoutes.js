085bd1332b8e849423a4888af36d028c505f784e
Farooque Azam <farooqueazam004@gmail.com>
2025-04-06 21:49:42 +0530

    added relevant pictures

--- backend/routes/homeRoutes.js
+++ backend/routes/homeRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { Router } from "express";
-
+import { isAuthenticated } from "../middleware/authMiddleware.js";
 const router = Router();
 
 router.get("/", (req, res) => {
     res.render("landing");
+});
+
+router.get("/profile", isAuthenticated, (req, res) => {
+    res.render("profile");
 });
 
 export default router;
